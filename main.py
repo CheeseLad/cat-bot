f8691f07f2fc245dbb506e38bd3fdb00e57225ce
CheeseLad <cheesescreativeserver@gmail.com>
2023-10-21 22:17:27 +0100

    working on tenor

--- main.py
+++ main.py
@@ -3,8 +3,34 @@
 import os
 import random
 import discord
-with open('token.txt', 'r') as f:
-    token = f.read()
+import requests
+import json
+
+with open('keys.txt', 'r') as f:
+    keys = f.read().splitlines()
+    token = keys[0]
+    tenor = keys[1]
+
+BASE_URL = 'https://api.tenor.com/v1/'
+ENDPOINT = 'random'
+params = {
+    'key': tenor,
+    'q': 'cat',
+    'limit': 1  
+}
+
+def tenor_cat():
+    response = requests.get(BASE_URL + ENDPOINT, params=params)
+    print(response)
+    if response.status_code == 200:
+      data = response.json()
+      gif_url = data['results'][0]['media'][0]['gif']['url']
+      print("Random Cat GIF URL:", gif_url)
+      return gif_url
+    else:
+      print(f"Error: {response.status_code}")
+      return None
+
 
 cat_gifs = []
 with open('cat-gifs.txt', 'r') as f:
@@ -14,12 +40,12 @@ with open('cat-gifs.txt', 'r') as f:
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
+        print('---------------------------------------------')
         member_count = sum(1 for _ in client.get_all_members())
         guild_count = sum(1 for _ in client.guilds)
-        client.activity = discord.Game(name=f'with {member_count} cats')
+        await client.change_presence(activity=discord.Game(name=f'with {member_count} cats in {guild_count} homes'))
         print(f'Playing with {member_count} cats in {guild_count} guilds')
-        print('------')
+        print('------------------')
     async def on_message(self, message):
         
         if message.author.id == self.user.id:
@@ -28,6 +54,7 @@ class MyClient(discord.Client):
         if  message.content.startswith('!cat'):
             print(f"{message.author} used '!cat'")
             response = random.choice(cat_gifs)
+            tenor_cat()
             await message.reply(f'{response}', mention_author=True)
 
     
@@ -38,4 +65,3 @@ intents.members = True
 
 client = MyClient(intents=intents)
 client.run(token)
-
